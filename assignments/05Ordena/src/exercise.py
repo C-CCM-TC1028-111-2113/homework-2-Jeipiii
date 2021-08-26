4fde220f6585fc66ad9c9d09f66f4e4febc1509e
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-26 18:49:37 -0500

    Update exercise.py
    x = int(input("Ingresa el primer número: "))
    y = int(input("Ingresa el segundo número: "))
    z = int(input("Ingresa el tercer número: "))
    
    if x > y and y > z :
        print( "The list is this ;" + str(x) + "" + str(y) + "" + str(z))
    if y > x and z > x :
        print ( "The list is this ;" + str(y) + "" + str(z) + "" + str(x))
    if z > y and x > y :
        print ( "The list is this ;" + str(z) + "" + str(x) +  "" + str(y))

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,7 +1,11 @@
-def main():
-    # Escribe el código adecuado para completar el programa
-    pass
+x = int(input("Ingresa el primer número: "))
+y = int(input("Ingresa el segundo número: "))
+z = int(input("Ingresa el tercer número: "))
 
-
-if __name__=='__main__':
-    main()
+if x > y and y > z :
+    print( "The list is this ;" + str(x) + "" + str(y) + "" + str(z))
+if y > x and z > x :
+    print ( "The list is this ;" + str(y) + "" + str(z) + "" + str(x))
+if z > y and x > y :
+    print ( "The list is this ;" + str(z) + "" + str(x) +  "" + str(y))
+    
