b5e86ba509f120b0339d6a65b45ee7513614594a
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-26 14:46:29 -0500

    Update exercise.py
    edad = int(input("Porfavor dame tu edad: "))
    identificacion = True
    
    if edad > 18 and indetificacion == True:
        print("You can have your id")
    elif edad > 18 and identificacion  == False:
        print("You cant have your id")
    else:
        print("You do not have any requisite")

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,8 +1,9 @@
+edad = int(input("Porfavor dame tu edad: "))
+identificacion = True
 
-def main():
-    #Escribe tu código debajo de esta línea
-    pass
-
-
-if __name__ == '__main__':
-    main()
+if edad > 18 and indetificacion == True:
+    print("You can have your id")
+elif edad > 18 and identificacion  == False:
+    print("You cant have your id")
+else:
+    print("You do not have any requisite")
