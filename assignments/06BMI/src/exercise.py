dd491f57464be2de41116743030605fd2b84b3fa
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-27 12:14:37 -0500

    Update exercise.py
    height =input("Please Input your height in meters\n")
    height= float (height)
    
    weight =float( input("Input your wieght in killograms\n"))
    BMI = weight/ ( height **2)
    
    print("The value for your BMI is : " + str(BMI) )
    
    
    if BMI > 40:
        print ( " You are Very severly obese")
    elif BMI >= 35:
        print( "  You are Severly obese")
    elif BMI >=30:
        print ("You are Moderately obese")
    elif BMI >= 25:
        print ("You are Overweight")
    elif BMI >=18.25:
        print ("You are Normal")
    elif BMI >= 16 :
        print ( "You are Underweight")
    elif BMI >=15:
        print ("You are severely underweight")
    elif BMI < 15:
        print ("You are Very severely underweight")

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,25 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
-if __name__=='__main__':
-    main()
+height =input("Please Input your height in meters\n")
+height= float (height)
+
+weight =float( input("Input your wieght in killograms\n"))
+BMI = weight/ ( height **2)
+
+print("The value for your BMI is : " + str(BMI) )
+
+
+if BMI > 40:
+    print ( " You are Very severly obese")
+elif BMI >= 35:
+    print( "  You are Severly obese")
+elif BMI >=30:
+    print ("You are Moderately obese")
+elif BMI >= 25:
+    print ("You are Overweight")
+elif BMI >=18.25:
+    print ("You are Normal")
+elif BMI >= 16 :
+    print ( "You are Underweight")
+elif BMI >=15:
+    print ("You are severely underweight")
+elif BMI < 15:
+    print ("You are Very severely underweight")
